6ef1a50f2f5d24d14a78058cebbe4f2478cda254
Yu Chang <chang@yus-macbook-pro.bc.edu>
2018-01-28 10:55:55 -0500

    commit to week 2 hw fork

--- You Are Awesome/ViewController.swift
+++ You Are Awesome/ViewController.swift
@@ -7,15 +7,48 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var image: UIImageView!
+    var awesomePlayer = AVAudioPlayer()
     var index = 0
+    var imageIndex = 0
+    var soundIndex = -1
+    var numOfImages = 9
+    var numOfSounds = 5
+    
     //Code below executives when app's code first loads
     override func viewDidLoad() {
         super.viewDidLoad()
         print("*** The viewDidLoad has executed at \(Date())")
+    }
+    
+    func playSound(soundName: String) {
+        if let sound = NSDataAsset(name: soundName) {
+            //check to see if it is a sound file
+            do {
+                try awesomePlayer = AVAudioPlayer(data: sound.data)
+                print(soundIndex)
+                awesomePlayer.play()
+            } catch {
+                //if sound.data is not acceptable
+                print("\(sound) could not be played.")
+            }
+        } else {
+            // if reading data didn't work, tell error
+            print("")
+        }
+    }
+    
+    func nonRepeatingRandom (lastNumber: Int, maxValue: Int) -> Int {
+        var newIndex = -1
+        repeat {
+            newIndex = Int(arc4random_uniform(UInt32(maxValue)))
+        } while lastNumber == newIndex
+        return newIndex
     }
 
     @IBAction func showMessagePressed(_ sender: UIButton) {
@@ -24,13 +57,32 @@ class ViewController: UIViewController {
                        "You are Awesome!",
                        "When the genius bar needs help, they call you!",
                        "You brighten my day!"]
-        messageLabel.text = message[index]
-        messageLabel.textColor = UIColor.orange
-        index += 1
         
-        if (index == message.count) {
-            index = 0
-        }
+        
+        //show a message
+        index = nonRepeatingRandom(lastNumber: index, maxValue: message.count)
+        messageLabel.text = message[index]
+        
+        //show an image
+        image.isHidden = false //show the image
+        imageIndex = nonRepeatingRandom(lastNumber: index, maxValue: numOfImages)
+        image.image = UIImage(named: "\(imageIndex+1)")
+        
+        //play a sound
+        soundIndex = nonRepeatingRandom(lastNumber: index, maxValue: numOfSounds)
+        let soundName = "Sound\(soundIndex)"
+        playSound(soundName: soundName)
+        
+//        var randomIndex = Int(arc4random_uniform(UInt32(messages.count))
+//        messageLabel.text = messages[randomIndex]
+        
+//        messageLabel.text = message[index]
+//        messageLabel.textColor = UIColor.orange
+//        index += 1
+//
+//        if (index == message.count) {
+//            index = 0
+//        }
         
 //        if messageLabel.text == message[0] {
 //            messageLabel.text = message[1]
